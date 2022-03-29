692424027c030de31446812b08564860e072392a
GarrenSouza <contato.garren@gmail.com>
2022-03-29 13:41:22 -0300

    feat: edge detection and gradient estimation

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,17 @@
 #define VIDEO_STREAM_EDGES "Edges (Canny)"
 #define VIDEO_STREAM_GRADIENT "Gradient (Sobel)"
 
+namespace Local{
+
+template<typename valueType>
+valueType clamp(valueType v, valueType lo, valueType hi){
+	return (v < lo)? lo : (v > hi)? hi : v;
+}
+
+}
 int main(int argc, const char *argv[])
 {
 	cv::Mat menu = cv::Mat(500, 500, CV_8UC3);
-	int count = 0;
 
 	cvui::init(MENU_WINDOW);
 
@@ -31,24 +38,24 @@ int main(int argc, const char *argv[])
 	bool shouldMirrorH = false;
 	bool getNegative = false;
 	bool applyGaussianBlur = false;
-	bool shouldDetectEdges = false;
+	bool detectEdges = false;
 	bool getGradientEstimative = false;
 
 	int rotations = 0;
 
 	double contrastAlpha = 1.0;
 	int brightnessBeta = 0.0;
+	int ddepth = CV_16S;
 
 	while (true) {
 		cv::Mat frame;
 		cap >> frame;
-		if(frame.empty()) break; // end of video stream
+		if(frame.empty()) break;
 
-		// Fill the frame with a nice color
 		menu = cv::Scalar(49, 52, 49);
 
 		cvui::trackbar(menu, 40, 30, 220, &kernelSize, (int)3, (int)33);
-		cvui::text(menu, 250, 50, "Gaussian kernel size");
+		cvui::text(menu, 250, 50, "Kernel size");
 
 		cvui::trackbar(menu, 40, 75, 220, &contrastAlpha, (double)0.5, (double)5.0);
 		cvui::text(menu, 250, 95, "Contrast");
@@ -56,28 +63,40 @@ int main(int argc, const char *argv[])
 		cvui::trackbar(menu, 40, 120, 220, &brightnessBeta, (int)0.0, (int)255.0);
 		cvui::text(menu, 250, 140, "Brightness");
 
-		if (cvui::button(menu, 40, 180, "Hello, world!")) count++;
-		if (cvui::button(menu, 40, 210, "Rotate 90")) rotations = (rotations + 1) & 3;
-		if (cvui::button(menu, 40, 240, "Mirror H")) shouldMirrorH = !shouldMirrorH;
-		if (cvui::button(menu, 40, 270, "Mirror V")) shouldMirrorV = !shouldMirrorV;
-		if (cvui::button(menu, 40, 300, "Detect Edges")) shouldDetectEdges = !shouldDetectEdges;
-		if (cvui::button(menu, 40, 330, "Get Gradient")) getGradientEstimative = !getGradientEstimative;
+		if (cvui::button(menu, 40, 225, "Rotate 90")) rotations = (rotations + 1) & 3;
+		if (cvui::button(menu, 40, 255, "Mirror H")) shouldMirrorH = !shouldMirrorH;
+		if (cvui::button(menu, 40, 285, "Mirror V")) shouldMirrorV = !shouldMirrorV;
 
+		cvui::checkbox(menu, 40, 345, "show edges", &detectEdges);
+		cvui::checkbox(menu, 40, 360, "show gradient", &getGradientEstimative);
 		cvui::checkbox(menu, 40, 375, "apply Gaussian Blur", &applyGaussianBlur);
 		cvui::checkbox(menu, 40, 390, "get negative", &getNegative);
 		cvui::checkbox(menu, 40, 405, "apply grayscale", &applyGrayscale);
 		cvui::checkbox(menu, 40, 420, "scale down", &shouldScaleDown);
 
 		frame.convertTo(frame, -1, contrastAlpha, brightnessBeta);
-		if(applyGaussianBlur) cv::GaussianBlur(frame, frame, cv::Size(kernelSize | 1, kernelSize | 1), 1.0, 1.0);
+		if (applyGaussianBlur) cv::GaussianBlur(frame, frame, cv::Size(kernelSize | 1, kernelSize | 1), 1.0, 1.0);
+		if (detectEdges){
+			cv::Mat edges;
+			cv::Canny(frame, edges, 30, 100, Local::clamp(2*(kernelSize - 1) - 1, 3, 7));
+			frame = edges;
+		}
+		if (getGradientEstimative && !detectEdges){
+			cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+			cv::Mat grad_x, grad_y;
+			cv::Mat abs_grad_x, abs_grad_y;
+			cv::Sobel(frame, grad_x, ddepth, 1, 0, kernelSize, 1.0f, 0.0f, cv::BORDER_DEFAULT);
+			cv::Sobel(frame, grad_y, ddepth, 0, 1, kernelSize, 1.0f, 0.0f, cv::BORDER_DEFAULT);
+			convertScaleAbs(grad_x, abs_grad_x);
+			convertScaleAbs(grad_y, abs_grad_y);
+			addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, frame);
+		}
 		if (getNegative) frame.convertTo(frame, -1, -1.0, 255);
-		if (applyGrayscale) cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+		if (applyGrayscale && !detectEdges) cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
 		if (shouldScaleDown) cv::resize(frame, frame, cv::Size(frame.size().width /2, frame.size().height /2));
 		for (size_t i = 0; i < rotations; i++) cv::rotate(frame, frame, cv::ROTATE_90_COUNTERCLOCKWISE);
 		if (shouldMirrorH) cv::flip(frame, frame, 1);
 		if (shouldMirrorV) cv::flip(frame, frame, 0);
-
-		cvui::printf(menu, 250, 200, 0.4, 0xff0000, "Button click count: %d", count);
 		
 		cvui::imshow(MENU_WINDOW, menu);		
 		cv::imshow(VIDEO_STREAM_WINDOW, frame);
